6074e733920c9bf61a069e756970f23c1e2011c3
Amadeus <138116141+sethdico@users.noreply.github.com>
2026-01-01 17:01:46 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,16 +7,17 @@ const ASSETS = [
   'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js',
   'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-tomorrow.min.css'
 ];
-
+ 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
+ 
 self.addEventListener('activate', (e) => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
 });
-
+ 
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
+ 
